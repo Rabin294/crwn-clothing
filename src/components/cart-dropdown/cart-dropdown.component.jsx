fb969b18eb06581ec7a3cd58bceaaff4cf1c87af
Rabin294 <bloomworld294@yahoo.com>
2021-08-02 11:56:30 +0800

    updating our application with reselect, replaced cart selectors with memoized selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import CustomButton from "../custom-buttom/button.component";
 import CartItem from "../cart-item/cart-item.component";
+import { selectCartItems } from "../../redux/cart/cart.selectors ";
 
 import "./cart-dropdown.component.styles.scss";
 
@@ -16,5 +17,5 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({ cartItems });
+const mapStateToProps = (state) => ({ cartItems: selectCartItems(state) });
 export default connect(mapStateToProps)(CartDropdown);
---
+++ src/redux/cart/cart.selectors .js
@@ -0,0 +1,18 @@
+import { createSelector } from "reselect";
+
+const selectCart = (state) => state.cart;
+
+export const selectCartItems = createSelector(
+  [selectCart],
+  (cart) => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  (cartItems) =>
+    cartItems.reduce(
+      (accumalatedQuentity, cartItem) =>
+        accumalatedQuentity + cartItem.quantity,
+      0
+    )
+);
